83ef98c1dafdc0f9ff14f5b632149f1e9d58333e
Angelino Valeta <angelinovaleta@gmail.com>
2023-12-04 23:33:42 +0100

    implement async error

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,125 +1,81 @@
+const asyncHandler = require("./../middleware/async")
+const ErrorResponse = require("./../utils/errorResponse")
 const Bootcamp = require("./../models/Bootcamp")
 
 // @desc        Get all bootcamps
 // @route       Get /api/bootcamps
 // @access      Public
-
-exports.getBootcamps = async (req, res) => {
-
-  try {
-    const data = await Bootcamp.find();
-
-    res.status(200).json({
-      success: true,
-      count: data.length,
-      data
-    })
-
-  } catch (err) {
-    res.status(400).json({ success: false })
-  }
-
-
-}
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const data = await Bootcamp.find();
+  res.status(200).json({
+    success: true,
+    count: data.length,
+    data
+  })
+})
 
 // @desc        Get single bootcamp
 // @route       Get /api/bootcamp/:id
 // @access      Public
-exports.getBootcamp = async (req, res) => {
-
-  try {
-
-    const data = await Bootcamp.findById(req.params.id)
-
-    if (!data) {
-      return res.status(404).json({ success: false, message: "Resource not found" })
-    }
-
-    res.status(200).json({
-      success: true,
-      data
-    })
-  } catch (err) {
-    res.status(500).json({ success: false })
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const data = await Bootcamp.findById(req.params.id)
+  if (!data) {
+    return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404))
   }
-}
 
+  res.status(200).json({
+    success: true,
+    data
+  })
+})
 
 // @desc        Create bootcamp
 // @route       Post /api/bootc amp
 // @access      private
-exports.createBootcamp = async (req, res) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body)
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body)
 
-    res.status(201).json({
-      success: true,
-      data: bootcamp
-    })
-
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      error: err
-    })
-  }
-}
-
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  })
+})
 
 // @desc        Update bootcamp
 // @route       Put /api/bootcamp/:id
 // @access      private
-exports.updateBootcamp = async (req, res) => {
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
-  try {
+  let data = await Bootcamp.findById(req.params.id)
 
-    let data = await Bootcamp.findById(req.params.id)
-
-    if (!data) {
-      return res.status(404).json({ success: false, message: "Resource not found" })
-    }
-
-    data = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { 
-      new: true,
-      runValidators: true
-    })
-
-    res.status(200).json({
-      success: true,
-      message: "Resource Updated",
-      data
-    })
-
-  } catch (err) {
-    res.status(500).json({ success: false, err})
+  if (!data) {
+    return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404))
   }
 
-}
+  data = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
 
+  res.status(200).json({
+    success: true,
+    message: "Resource Updated",
+    data
+  })
+})
 
 // @desc        Delete bootcamp
 // @route       Delete /api/bootcamp/:id
 // @access      private
-exports.deleteBootcamp = async (req, res) => {
-
-  try {
-
-    let data = await Bootcamp.findById(req.params.id)
-
-    if (!data) {
-      return res.status(404).json({ success: false, message: "Resource not found" })
-    }
-
-    data = await Bootcamp.findByIdAndDelete(req.params.id)
-
-    res.status(200).json({
-      success: true,
-      message: "Resource deleted"
-    })
-
-  } catch (err) {
-    res.status(500).json({ success: false })
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  let data = await Bootcamp.findById(req.params.id)
+  if (!data) {
+    return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`))
   }
-
-}
+  await Bootcamp.findByIdAndDelete(req.params.id)
+  res.status(200).json({
+    success: true,
+    message: "Resource deleted"
+  })
+})
 
